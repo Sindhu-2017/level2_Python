008e18d950078dc088edcb48adeeedfa53fc6377
unknown <sindhujas388@gmail.com>
2026-09-23 18:25:18 +0530

    initial commit

---
+++ Task 1/shopping_cart_calculator.py
@@ -0,0 +1,19 @@
+# 5. Shopping Cart Calculator  
+
+# Given multiple product prices: 
+
+# Enter number of products: 5 
+
+# Accept the prices using a loop. 
+
+# Calculate: 
+
+# Subtotal  
+
+# Discount  
+
+# Tax  
+
+# Final amount  
+
+# Apply different discounts based on the subtotal. 
